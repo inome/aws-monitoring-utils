4115c0c186f491fda60f75066da765b3ea6249ce
Micah Huff <mhuff@inome.com>
2014-10-31 07:59:20 -0700

    Splice cron that will run daily

--- utils/accesskeys.py
+++ utils/accesskeys.py
@@ -5,20 +5,29 @@ CONNECTION_KEYS = ["/helix-aws/aws_access_key", "/helix-aws/aws_secret_access_ke
                       "/helix-aws/librato_username", "/helix-aws/pingdom_api_key", "/helix-aws/pingdom_password",
                       "/helix-aws/pingdom_username", "/helix-aws/aws_regions"]
 
-def getAccessPropertiesFromConfigService(env="DEV", apiKey=""):
+def get_key_from_configservice(env="DEV", api_key="", key="", default_value=None):
+    url = "http://config.vs.intelius.com:8080/configuration/1.0/get?env=%s&key=%s" % (env, key)
+    if len(api_key) > 0:
+        url += "&callerid=%s" % api_key
+    debug(url)
+    try:
+        data = requests.get(url)
+        datajson = data.json()
+        if data.status_code == 200 and "value" in datajson:
+            return datajson["value"]
+        elif "value" not in datajson:
+            return default_value
+    except Exception as e:
+        print("UNABLE TO RETRIEVE DATA FROM THE CONFIGURATION SERVICE")
+        print(e)
+
+def getAccessPropertiesFromConfigService(env="DEV", api_key="", strip_helix_prefix=True):
     returnmap = {}
     for key in CONNECTION_KEYS:
-        url = "http://config.vs.intelius.com:8080/configuration/1.0/get?env=%s&key=%s" % (env,key)
-        if len(apiKey) > 0:
-            url += "&callerid=%s" % apiKey
-        debug(url)
-        try:
-            data = requests.get(url)
-            datajson = data.json()
-            if data.status_code == 200 and "value" in datajson:
-                returnmap[key.replace("/helix-aws/", "")] = datajson["value"]
-        except Exception as e:
-            print("UNABLE TO RETRIEVE DATA FROM THE CONFIGURATION SERVICE")
-            print(e)
-            returnmap = None
+        value = get_key_from_configservice(env, api_key, key)
+        if value is not None:
+            if strip_helix_prefix:
+                returnmap[key.replace("/helix-aws/", "")] = value
+            else:
+                returnmap[key] = value
     return returnmap
